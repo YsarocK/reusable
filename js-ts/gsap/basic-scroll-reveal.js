21cd3ac30fefc8c335d548c66b96b340590e5a04
Etienne <etienne.moureton@unflux.fr>
2022-07-14 18:30:20 +0200

    add scroll reveal GSAP

---
+++ js-ts/gsap/basic-scroll-reveal.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll('.reveal-wrapper')
+const length = sections.length
+
+for(let i = 0; i < length; i ++) {
+  const section = sections[i]
+  const childs = section.querySelectorAll('.reveal')
+  const tl = gsap.timeline({ paused: true })
+  for(let i = 0; i < childs.length; i ++) {
+    tl.from(childs[i], {
+      opacity: 0,
+      y: 20,
+      duration: 0.5,
+      ease: "power2.out",
+    }, "-=0.3")
+  }
+
+  ScrollTrigger.create({  
+    trigger: section,
+    start: 'top center+=120',
+    animation: tl,
+  })
+}
