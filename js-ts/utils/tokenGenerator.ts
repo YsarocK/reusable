4bcd527f3ec9a08dc66340202d7ee2c66dcc99fb
Etienne <etienne.moureton@unflux.fr>
2022-07-14 19:02:29 +0200

    token generator

---
+++ js-ts/utils/tokenGenerator.ts
@@ -0,0 +1,11 @@
+const generateToken = (length: number) => {
+  const a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
+  let b: Array<string> = [];  
+  for (var i=0; i < length; i++) {
+      var j = (Math.random() * (a.length-1)).toFixed(0);
+      b[i] = a[j];
+  }
+  return b.join("");
+}
+
+console.log(generateToken(9))
